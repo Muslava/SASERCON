3281ec10ef6c58e9999bd7bd45a4f33d16c1b594
Muslava <30022704+Muslava@users.noreply.github.com>
2019-05-14 15:20:46 -0500

    Avance
    > Agregada la tabla de interfaces.
    > Ordenadas las metáforas.
    > Nuevas interfaces agregadas.

---
+++ Proyecto/Escritorio/SASERCON/src/interfaces/ConexionMySQL.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+/**
+ *
+ * @author darki
+ */
+import java.sql.*;
+import javax.swing.JOptionPane;
+//import com.mysql.jdbc.Connection;
+public class ConexionMySQL {
+    public String db="TercerParcial";
+    public String url="jdbc:mysql://localhost:3306/"+db;
+    public String usuario="root";
+    public String pass="";
+   
+    public ConexionMySQL(){
+    }
+    
+    public Connection Conectar(){
+        Connection enlace=null;
+        try{
+            Class.forName("org.gjt.mm.mysql.Driver");
+            enlace=(Connection)DriverManager.getConnection(this.url,this.usuario,this.pass);
+        }catch(Exception ex){
+            JOptionPane.showMessageDialog(null, ex);
+        }
+          return enlace;  
+            
+        }
+    
+}
