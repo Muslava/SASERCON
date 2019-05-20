af66d7ed9b0fddbc972239f619f818c2c755e947
Muslava <30022704+Muslava@users.noreply.github.com>
2019-05-20 08:43:21 -0500

    2

--- Proyecto/Escritorio/SASERCON/src/interfaces/IngresarServicio.java
+++ Proyecto/Escritorio/SASERCON/src/interfaces/IngresarServicio.java
@@ -3,18 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Aldo;
+package interfaces;
 
 /**
  *
  * @author darki
  */
-public class ingresarServicios extends javax.swing.JFrame {
+public class IngresarServicio extends javax.swing.JFrame {
 
     /**
      * Creates new form ingresarServicios
      */
-    public ingresarServicios() {
+    public IngresarServicio() {
         initComponents();
     }
 
@@ -479,20 +479,23 @@ public class ingresarServicios extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ingresarServicios.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(IngresarServicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ingresarServicios.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(IngresarServicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ingresarServicios.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(IngresarServicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ingresarServicios.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(IngresarServicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ingresarServicios().setVisible(true);
+                new IngresarServicio().setVisible(true);
             }
         });
     }
